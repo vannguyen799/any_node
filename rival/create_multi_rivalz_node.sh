773dc5813fe5f54313e7b4c607d82b51405c9d16
Nguyen Ba Van <nguyenbavan.dev@gmail.com>
2024-12-25 06:34:47 -0800

    --

--- rival/create_multi_rivalz_node.sh
+++ rival/create_multi_rivalz_node.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+log_file="rivalz_node_create_log.csv"
+
+if [ ! -f "$log_file" ]; then
+    echo "timestamp,screen_name,wallet_address,storage_value" > "$log_file"
+fi
 
 echo 'wallet_address:'
 read wallet_address
 echo 'storage_value:'
 read storage_value
-echo 'node_number:'
+echo 'How many node to create:'
 read node_number
 
-if [ ! -f "create_rivalz_node.sh" ]; then
-  curl -O https://raw.githubusercontent.com/vannguyen799/any_node/refs/heads/master/rival/create_rivalz_node.sh
-  chmod +x create_rivalz_node.sh
-fi
+
+# curl -O https://raw.githubusercontent.com/vannguyen799/any_node/refs/heads/master/rival/create_multi_rivalz_node.sh && chmod +x create_multi_rivalz_node.sh && ./create_multi_rivalz_node.sh
+curl -O https://raw.githubusercontent.com/vannguyen799/any_node/refs/heads/master/rival/create_rivalz_node.sh
+chmod +x create_rivalz_node.sh
 
 for i in $(seq 1 $node_number); do
   name="rival-$(date +%s)"
-
+  echo "screen-$name start"
   screen -dmS "$name" bash -c "echo -e '$wallet_address\n$storage_value' | ./create_rivalz_node.sh"
+
+  echo "$(date +%Y-%m-%dT%H:%M:%S),$name,$wallet_address,$storage_value" >> "$log_file"
+  sleep 0.3
 done
